0f4a6b43a66f182a43a52db706bffb092eb03af5
Zeina <145121493+zeexqn@users.noreply.github.com>
2024-10-09 13:49:56 +0700

    Add files via upload

---
+++ Latihan1.sql
@@ -0,0 +1,7 @@
+
+--Menambahkan tabel---
+	INSERT INTO Mahasiswa (ID, NIM, Nama, Jurusan, TanggalLahir)
+	VALUES (12,233040146, 'DeraZeina', 'Teknik Informatika', '2004-11-11'),(13,233040120, 'Ilona', 'Teknik Informatika', '2004-06-20'),(14,233040144, 'Dinda', 'Teknik Informatika', '2004-06-13'),(15,233040129, 'milda', 'Teknik Informatika', '2004-09-21'),(16,233040118, 'ana', 'Teknik Informatika', '2005-07-18'),(17,233040119, 'mul', 'Teknik Informatika', '2005-10-17');
+
+	SELECT *
+	FROM [dbo].Mahasiswa
